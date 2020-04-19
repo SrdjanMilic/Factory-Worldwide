b2f465ff0a387549a4217bfc2da03664974ba721
Srdjan Milic <srdjanrmilic@gmail.com>
2020-04-19 09:36:29 +0200

    rules tweak

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "semi": ["error", "always"]
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "space-before-function-paren": ["error", "never"]
   }
 }
