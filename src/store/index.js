ed0a156ad65fd94adbe69740f6cbf9e2c3afab00
Srdjan Milic <srdjanrmilic@gmail.com>
2021-04-22 08:58:21 +0200

    vuex implementation

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    changes: [],
+    timer: [0, 0, 0],
+    fields: ['A', 'B', 'C'],
+    signs: ['+', '-'],
+    randomSign: ['+', '+', '+'],
+    randomNumbers: []
+  },
+  mutations: {
+    replaceNumbersArray(state) {
+      // replace random A, B, C... numbers at time interval
+      const A = Number((Math.random() + 1).toFixed(2)); // first number A
+      const B = Number((Math.random() + 1).toFixed(2)); // first number B
+      const C = Number((Math.random() + 1).toFixed(2)); // first number C
+      state.randomNumbers.splice(0, 3, A, B, C);
+      console.log(state.randomNumbers);
+    }
+  }
+});
+
+export default store;
