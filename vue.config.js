827d60597369abdfa7ca867a3546449d4843b0bf
Srdjan Milic <srdjanrmilic@gmail.com>
2021-04-23 06:14:54 +0200

    path to stylesheet change

--- vue.config.js
+++ vue.config.js
@@ -2,24 +2,24 @@ module.exports = {
   pages: {
     index: {
       // entry for the page
-      entry: "src/main.js",
+      entry: 'src/main.js',
       // the source template
-      template: "public/index.html",
+      template: 'public/index.html',
       // output as dist/index.html
-      filename: "index.html",
+      filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "Table Statistics",
+      title: 'Table Statistics',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
-      chunks: ["chunk-vendors", "chunk-common", "index"]
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
     }
   },
   css: {
     loaderOptions: {
       sass: {
         // eslint-disable-next-line
-        prependData: `@import "@/assets/styles/main.scss";`
+        prependData: `@import "@/assets/css/main.scss";`
       }
     }
   }
